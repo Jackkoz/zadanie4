3442f6faf9a6bac28dfcfb4ff19371f4e3e12707
Jacek Koziński <ubuntu@ubuntu-VirtualBox.(none)>
2013-12-01 02:29:36 +0100

    Poprawka split

--- private/holding.hh
+++ private/holding.hh
@@ -21,60 +21,62 @@ typedef Company<0, 1, 0> Hunting_shop;
 typedef Company<0, 0, 1> Exchange_office;
 
 /*
- * Typ reprezentujący firmę powstałą z połączenia firm C1 i C2. Struktura 
+ * Typ reprezentujący firmę powstałą z połączenia firm C1 i C2. Struktura
  * powinna zawierać publiczną definicję type, opisującą nową firmę.
  */
-template<class C1, class C2> 
-struct add_comp 
+template<class C1, class C2>
+struct add_comp
 {
 public:
-	//TODO zakres (MAX_UINT) ?? 
-	typedef Company<C1::accNumber + C2::accNumber, 
-					C1::hunNumber + C2::hunNumber, 
+	//TODO zakres (MAX_UINT) ??
+	typedef Company<C1::accNumber + C2::accNumber,
+					C1::hunNumber + C2::hunNumber,
 					C1::excNumber + C2::excNumber> type;
 };
 
 /*
- * Typ reprezentujący firmę C1 pomniejszoną o firmę C2. Struktura powinna 
+ * Typ reprezentujący firmę C1 pomniejszoną o firmę C2. Struktura powinna
  * zawierać publiczną definicję type, opisującą nową firmę.
  */
-template<class C1, class C2> 
-struct remove_comp 
+template<class C1, class C2>
+struct remove_comp
 {
 public:
-	typedef Company<(C1::accNumber - C2::accNumber >= 1 ? C1::accNumber - C2::accNumber : 0), 
-					(C1::hunNumber - C2::hunNumber >= 1 ? C1::hunNumber - C2::hunNumber : 0), 
+	typedef Company<(C1::accNumber - C2::accNumber >= 1 ? C1::accNumber - C2::accNumber : 0),
+					(C1::hunNumber - C2::hunNumber >= 1 ? C1::hunNumber - C2::hunNumber : 0),
 					(C1::excNumber - C2::excNumber >= 1 ? C1::excNumber - C2::excNumber : 0)> type;
 
 };
 
 /*
  * Typ reprezentujący firmę powstałą z firmy C przez powiększenie n razy liczby
- * poszczególnych przedsiębiorstw. Struktura powinna zawierać publiczną 
+ * poszczególnych przedsiębiorstw. Struktura powinna zawierać publiczną
  * definicję type, opisującą nową firmę.
  */
-template<class C, unsigned int n> 
+template<class C, unsigned int n>
 struct multiply_comp
-{	
+{
 public:
 	//TODO zakres (MAX_UINT) ??
 	typedef Company<C::accNumber * n, C::hunNumber * n, C::excNumber * n> type;
-	
+
 };
 
 /*
- * Potrzebny jest również typ, który ułatwi podział firmy na n mniejszych. Ten 
+ * Potrzebny jest również typ, który ułatwi podział firmy na n mniejszych. Ten
  * typ reprezentuje firmę powstałą z firmy C przez pomniejszenie n razy liczby
- * poszczególnych przedsiębiorstw. Struktura powinna zawierać publiczną 
+ * poszczególnych przedsiębiorstw. Struktura powinna zawierać publiczną
  * definicję type, opisującą nową firmę.
  * TODO Dzielimy całkowicie, czy sprawdzamy podzielność przez asercję?
+ * - dzielenie jest zawsze całkowitoliczbowe, resztę odrzucamy;
+ * - jeśli dzielnik ma wartość zero, to wynikiem dzielenia ma być zero;
  */
-template<class C, unsigned int n> 
+template<class C, unsigned int n>
 struct split_comp
-{	
+{
 public:
-	typedef Company<C::accNumber / n, C::hunNumber / n, C::excNumber / n> type;
-	
+	typedef Company<n >= 1 ? C::accNumber / n : 0, n >= 1 ? C::hunNumber / n : 0, n >= 1 ? C::excNumber / n : 0> type;
+
 };
 
 /*
@@ -82,7 +84,7 @@ public:
  * przedsiębiorstw wchodzących w skład firmy. Napisz typ opisujący efekt takiej
  * operacji.
  */
-template<class C> 
+template<class C>
 struct additive_expand_comp
 {
 public:
@@ -90,21 +92,21 @@ public:
 };
 
 /*
- * Czasami zdarza się, że musimy zamknąć po jednym przedsiębiorstwie każdego 
- * typu, dlatego przygotuj strukturę, która będzie opisywać efekt takiej 
- * operacji. 
+ * Czasami zdarza się, że musimy zamknąć po jednym przedsiębiorstwie każdego
+ * typu, dlatego przygotuj strukturę, która będzie opisywać efekt takiej
+ * operacji.
  */
-template<class C> 
+template<class C>
 struct additive_rollup_comp
 {
 	static_assert(C::accNumber > 0, "Negative accountancy number.");
 	static_assert(C::hunNumber > 0, "Negative hunting shops number.");
 	static_assert(C::excNumber > 0, "Negative exchange offices number.");
-	
+
 public:
-	typedef Company<C::accNumber >= 1 ? C::accNumber - 1 : 0, 
-					C::hunNumber >= 1 ? C::hunNumber - 1 : 0, 
-					C::excNumber >= 1 ? C::excNumber - 1 : 0> type;	
+	typedef Company<C::accNumber >= 1 ? C::accNumber - 1 : 0,
+					C::hunNumber >= 1 ? C::hunNumber - 1 : 0,
+					C::excNumber >= 1 ? C::excNumber - 1 : 0> type;
 };
 
 
@@ -113,26 +115,26 @@ class Group
 {
 private:
 	unsigned int size;
-	
+
 	unsigned int accValue;
 	unsigned int hunValue;
 	unsigned int excValue;
-	
+
 public:
 	/*
-	 * Wszystkie błędy, które niefortunnie mogłyby się znaleźć w przygotowanym 
-	 * przez Ciebie programie mogłyby kosztować nas bardzo dużo pieniędzy, 
-	 * dlatego w celu ułatwienia testowania klasa Group<Company> powinna 
-	 * zawierać publiczną definicję typu reprezentującego strukturę pojedynczej 
-	 * firmy o nazwie company_type oraz zmienną statyczną company typu 
+	 * Wszystkie błędy, które niefortunnie mogłyby się znaleźć w przygotowanym
+	 * przez Ciebie programie mogłyby kosztować nas bardzo dużo pieniędzy,
+	 * dlatego w celu ułatwienia testowania klasa Group<Company> powinna
+	 * zawierać publiczną definicję typu reprezentującego strukturę pojedynczej
+	 * firmy o nazwie company_type oraz zmienną statyczną company typu
 	 * company_type.
 	 */
 	typedef C company_type;
 	static const company_type company;
 
-	
+
 	/*
-	 * Konstruktor wywołany bez parametrów tworzy grupę składającą się tylko z 
+	 * Konstruktor wywołany bez parametrów tworzy grupę składającą się tylko z
 	 * jednej firmy.
 	 */
 	Group() :
@@ -141,7 +143,7 @@ public:
 	}
 
 	/*
-	 * Konstruktor wywołany z jednym parametrem tworzy grupę składającą się 
+	 * Konstruktor wywołany z jednym parametrem tworzy grupę składającą się
 	 * dokładnie z k firm o identycznej strukturze.
 	 */
 	Group(unsigned int k) :
@@ -153,7 +155,7 @@ public:
 	}
 
 	/*
-	 * Konstruktor tworzący nową grupę, która jest identyczna z tą podaną jako 
+	 * Konstruktor tworzący nową grupę, która jest identyczna z tą podaną jako
 	 * argument.
 	 */
 	Group(Group<C> const &group) :
@@ -174,7 +176,7 @@ public:
 
 	/*
 	 * Metody pozwalające ustalać nową wartość biura rachunkowego,
-	 * sklepu myśliwskiego oraz kantoru. Początkowa wartość pojedynczego kantoru 
+	 * sklepu myśliwskiego oraz kantoru. Początkowa wartość pojedynczego kantoru
 	 * to 50, sklepu myśliwskiego to 150, a biura rachunkowego to 15.
 	 */
 	void set_acc_val(unsigned int i)
@@ -191,7 +193,7 @@ public:
 	}
 
 	/*
-	 * Metody pozwalające odczytać wartość biura rachunkowego, sklepu 
+	 * Metody pozwalające odczytać wartość biura rachunkowego, sklepu
 	 * myśliwskiego oraz kantoru.
 	 */
 	unsigned int get_acc_val() const
@@ -208,7 +210,7 @@ public:
 	}
 
 	/*
-	 * Metoda zwracające wartość danej grupy, czyli sumę wartośći wszystkich 
+	 * Metoda zwracające wartość danej grupy, czyli sumę wartośći wszystkich
 	 * przedsiębiorstw wchodzących w skład tej grupy.
 	 */
 	unsigned int get_value() const
