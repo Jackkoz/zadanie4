f1e77195fbd5fe87c3368ba9b075deaaba02dc95
Jacek Koziński <jacek.koz@gmail.com>
2013-12-04 20:03:20 +0100

    Refactors

--- private/holding.hh
+++ private/holding.hh
@@ -236,7 +236,7 @@ public:
 
     Group<C>& operator-= (const Group<C>& rhs)
     {
-        unsigned int new_size = size > rhs.size ? size - rhs.size : 0;
+        unsigned int new_size = subtract(size, rhs.size);
         if (new_size == 0) {
             accValue = 0;
             hunValue = 0;
@@ -244,18 +244,15 @@ public:
         } else {
             unsigned int lhs_acc_worth = size * accValue;
             unsigned int rhs_acc_worth = rhs.size * rhs.accValue;
-            accValue = lhs_acc_worth > rhs_acc_worth ?
-                      (lhs_acc_worth - rhs_acc_worth) / new_size : 0;
+            accValue = subtract(size * accValue, rhs.size * rhs.accValue) / new_size;
 
             unsigned int lhs_hun_worth = size * hunValue;
             unsigned int rhs_hun_worth = rhs.size * rhs.hunValue;
-            hunValue = lhs_hun_worth > rhs_hun_worth ?
-                      (lhs_hun_worth - rhs_hun_worth) / new_size : 0;
+            hunValue = subtract(size * hunValue, rhs.size * rhs.hunValue) / new_size;
 
             unsigned int lhs_exc_worth = size * excValue;
             unsigned int rhs_exc_worth = rhs.size * rhs.excValue;
-            excValue = lhs_exc_worth > rhs_exc_worth ?
-                      (lhs_exc_worth - rhs_exc_worth) / new_size : 0;
+            excValue = subtract(size * excValue, rhs.size * rhs.excValue) / new_size;
         }
         size = new_size;
 
@@ -272,53 +269,27 @@ public:
     Group<C>& operator*= (const int n)
     {
         size *= n;
-
-        if (n == 0)
-        {
-            accValue = 0;
-            hunValue = 0;
-            excValue = 0;
-        } else
-        {
-            accValue /= n;
-            hunValue /= n;
-            excValue /= n;
-        }
+        accValue = divide(accValue, n);
+        hunValue = divide(hunValue, n);
+        excValue = divide(excValue, n);
 
         return *this;
     }
 
     Group<C>& operator/= (const int n)
     {
-        if (n == 0)
-        {
-            size = 0;
-            accValue = 0;
-            hunValue = 0;
-            excValue = 0;
-        } else
-        {
-            size /= n;
-            accValue *= n;
-            hunValue *= n;
-            excValue *= n;
-        }
+        size = divide(size, n);
+        accValue *= n;
+        hunValue *= n;
+        excValue *= n;
 
         return *this;
     }
-
-    /*
-     * Operatory porównywania grup firm: ==, !=, <, >, <=, >=. Pamiętaj, że w
-     * krytycznych sytuacjach nie jest istotne, ile mamy biur rachunkowych. Liczy się
-     * tylko liczba kantorów (czyli kasa) oraz sklepów myśliwskich (broń). Posiadanie
-     * większej liczby przedsiębiorstw tych dwóch typów czyni nas silniejszymi od
-     * przeciwnika. Porządek na grupach nie jest liniowy.
-     */
-
 };
 
-//***Non-member function overloads***
-
+/*
+ * Pozostałe operatory arytmetyczne
+ */
 template<class C>
 Group<C> operator+ (const Group<C>& lhs, const Group<C>& rhs)
 {
@@ -351,6 +322,13 @@ Group<C> operator/ (const Group<C>& lhs, const int n)
     return new_group;
 }
 
+/*
+ * Operatory porównywania grup firm: ==, !=, <, >, <=, >=. Pamiętaj, że w
+ * krytycznych sytuacjach nie jest istotne, ile mamy biur rachunkowych. Liczy się
+ * tylko liczba kantorów (czyli kasa) oraz sklepów myśliwskich (broń). Posiadanie
+ * większej liczby przedsiębiorstw tych dwóch typów czyni nas silniejszymi od
+ * przeciwnika. Porządek na grupach nie jest liniowy.
+ */
 template<class C, class D>
 bool operator==(const Group<C>& lhs, const Group<D>& rhs)
 {
@@ -390,6 +368,9 @@ bool operator>(const Group<C>& lhs, const Group<D>& rhs)
     return lhs >= rhs && lhs != rhs;
 }
 
+/*
+ * Operator << wypisujący na strumień opis grupy.
+ */
 template<class C>
 std::ostream& operator<< (std::ostream& os, const Group<C>& rhs)
 {
@@ -402,17 +383,20 @@ std::ostream& operator<< (std::ostream& os, const Group<C>& rhs)
     return os;
 }
 
-// Bardzo liczymy również na to, że napiszesz następujące funkcje globalne.
-
+/*
+ * Bardzo liczymy również na to, że napiszesz następujące funkcje globalne.
+ */
 template<class C>
 Group<typename additive_expand_comp<C>::type> const
 additive_expand_group(Group<C> const &s1)
 {
     return Group<typename additive_expand_comp<C>::type>(s1);
 }
-// Zwiększa o jeden liczbę przedsiębiorstw (wszystkich typów) wchodzących w skład
-// każdej firmy należącej do grupy s1, nie zmieniając wartości pojedynczego
-// przedsiębiorstwa.
+/*
+ * Zwiększa o jeden liczbę przedsiębiorstw (wszystkich typów) wchodzących w skład
+ * każdej firmy należącej do grupy s1, nie zmieniając wartości pojedynczego
+ * przedsiębiorstwa.
+ */
 
 template<class C>
 Group<typename multiply_comp<C, 10>::type> const
@@ -420,9 +404,11 @@ multiplicative_expand_group(Group<C> const &s1)
 {
     return Group<typename multiply_comp<C, 10>::type>(s1);
 }
-// Zwiększa dziesięciokrotnie liczbę przedsiębiorstw (wszystkich typów) wchodzących
-// w skład każdej firmy należącej do grupy s1, nie zmieniając wartości pojedynczego
-// przedsiębiorstwa.
+/*
+ * Zwiększa dziesięciokrotnie liczbę przedsiębiorstw (wszystkich typów) wchodzących
+ * w skład każdej firmy należącej do grupy s1, nie zmieniając wartości pojedynczego
+ * przedsiębiorstwa.
+ */
 
 template<class C>
 Group<typename additive_rollup_comp<C>::type> const
@@ -430,9 +416,11 @@ additive_rollup_group(Group<C> const &s1)
 {
     return Group<typename additive_rollup_comp<C>::type>(s1);
 }
-// Zmniejsza o jeden liczbę przedsiębiorstw (wszystkich typów) wchodzących w skład
-// każdej firmy należącej do grupy s1, nie zmieniając wartości pojedynczego
-// przedsiębiorstwa.
+/*
+ * Zmniejsza o jeden liczbę przedsiębiorstw (wszystkich typów) wchodzących w skład
+ * każdej firmy należącej do grupy s1, nie zmieniając wartości pojedynczego
+ * przedsiębiorstwa.
+ */
 
 template<class C>
 Group<typename split_comp<C, 10>::type> const
@@ -440,9 +428,11 @@ multiplicative_rollup_group(Group<C> const &s1)
 {
     return Group<typename split_comp<C, 10>::type>(s1);
 }
-// Zmniejsza dziesięciokrotnie liczbę przedsiębiorstw (wszystkich typów)
-// wchodzących w skład każdej firmy należącej do grupy s1, nie zmieniając wartości
-// pojedynczego przedsiębiorstwa.
+/*
+ * Zmniejsza dziesięciokrotnie liczbę przedsiębiorstw (wszystkich typów)
+ * wchodzących w skład każdej firmy należącej do grupy s1, nie zmieniając wartości
+ * pojedynczego przedsiębiorstwa.
+ */
 
 template<class C1, class C2, class C3>
 bool
@@ -450,9 +440,11 @@ solve_auction(Group<C1> const &g1, Group<C2> const &g2, Group<C3> const &g3)
 {
     return (g1 > g2 && g1 > g3) || (g2 > g1 && g2 > g3) || (g3 > g1 && g3 > g2);
 }
-// Funkcja, która pomoże nam określać, czy możliwe jest wyłonienie zwycięzcy
-// przetargu (nie zawsze jest to możliwe) w przypadku, gdy startują w nim grupy g1,
-// g2 oraz g3. Zwycięzcą zostaje grupa, która jest największa w sensie porządku
-// zdefiniowanego na grupach.
+/*
+ * Funkcja, która pomoże nam określać, czy możliwe jest wyłonienie zwycięzcy
+ * przetargu (nie zawsze jest to możliwe) w przypadku, gdy startują w nim grupy g1,
+ * g2 oraz g3. Zwycięzcą zostaje grupa, która jest największa w sensie porządku
+ * zdefiniowanego na grupach.
+ */
 
 #endif
